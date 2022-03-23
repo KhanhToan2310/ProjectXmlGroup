0d2f2b938213c0c5e52c7f0f3062ed7ed41f52ad
KhanhToan2310 <khanhtoantruong196@gmail.com>
2022-03-23 09:46:28 +0700

    config src

--- ProjectXml/src/main/java/com/example/demo/controller/AdminController.java
+++ ProjectXml/src/main/java/com/example/demo/controller/AdminController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
 
+	@RequestMapping(value = "/selectPostList")
+	public String selectPostList() {
+		return "ADMIN/examples/postList";
+	}
 }
