c03cf65c2ce0e9f9c4fafa306951f755c3401b73
duc140032 <dangcongduc090@gmail.com>
2022-03-23 09:38:51 +0700

    commit html admin

--- ProjectXml/src/main/java/com/example/demo/controller/AdminController.java
+++ ProjectXml/src/main/java/com/example/demo/controller/AdminController.java
@@ -1,8 +1,63 @@
 package com.example.demo.controller;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Account;
+import com.example.demo.model.Role;
+import com.example.demo.service.AccountService;
+import com.example.demo.service.RoleService;
 
 @Controller
 public class AdminController {
 
+	@Autowired
+	private AccountService accountService;
+
+	@Autowired
+	private RoleService roleService;
+
+	// selectAccountView
+	@RequestMapping("/selectAccountList")
+	public String selectAccountList(Model model)
+			throws FileNotFoundException, UnsupportedEncodingException, XMLStreamException {
+
+		Map<String, String> selectRoleMap = selectRoleMap();
+		List<Account> accountList = accountService.ReadListAccount();
+		List<Account> list = new ArrayList<Account>();
+		
+		for (Account account : accountList) {
+			if (!"1".equals(account.getRole())) {
+				list.add(account);
+			}
+		}
+		
+		model.addAttribute("listAccount", list);
+		model.addAttribute("selectRoleMap", selectRoleMap);
+		return "ADMIN/examples/tablesAccounts";
+	}
+
+	private Map<String, String> selectRoleMap() {
+		Map<String, String> map = new HashMap<String, String>();
+		List<Role> roles = roleService.ReadListRole();
+
+		for (Role role : roles) {
+			map.put(role.getId(), role.getName());
+		}
+
+		return map;
+	}
 }
